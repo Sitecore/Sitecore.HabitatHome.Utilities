c008e5f2bdcaf33500c70e38d2f2a736921f6ce7
Jean-François Larente <jfl@sitecore.net>
2018-06-21 16:29:11 +0000

    Merged PR 848: Fixing sas token issue

--- Azure/CreateVHDFromSnapshot.ps1
+++ Azure/CreateVHDFromSnapshot.ps1
@@ -48,13 +48,22 @@ foreach ($region in $regions) {
     Write-Host "Copying VHDs - this will take a while... " -ForegroundColor Green
     Write-Host "Copying OS Disk" -ForegroundColor Green
     Write-Host "Generating SAS tokens for snapshot(s)..." -ForegroundColor Green
-    $osSAS = Grant-AzureRmSnapshotAccess -ResourceGroupName $snapshotResourceGroupName -SnapshotName $osSnapshotName -DurationInSecond $sasExpiryDuration -Access Read
-    $osSAS=$(az snapshot grant-access -g $snapshotResourceGroupName -n $osSnapshotName --duration-in-seconds $sasExpiryDuration -o tsv)
+  
+    $DebugPreference = 'Continue'
+  
+    $result = Grant-AzureRmSnapshotAccess -ResourceGroupName $snapshotResourceGroupName -SnapshotName $osSnapshotName -Access 'Read' -DurationInSecond $sasExpiryDuration 5>&1
+  
+    $DebugPreference = 'SilentlyContinue'
+  
+    $sasUri = ((($result | where {$_ -match "accessSAS"})[-1].ToString().Split("`n") | where {$_ -match "accessSAS"}).Split(' ') | where {$_ -match "https"}).Replace('"','')
+  
+    #$osSAS = Grant-AzureRmSnapshotAccess -ResourceGroupName $snapshotResourceGroupName -SnapshotName $osSnapshotName -DurationInSecond $sasExpiryDuration -Access Read
+   # $osSAS=$(az snapshot grant-access -g $snapshotResourceGroupName -n $osSnapshotName --duration-in-seconds $sasExpiryDuration -o tsv)
 
     #$osSAS = Grant-AzureRmSnapshotAccess -ResourceGroupName $snapshotResourceGroupName -SnapshotName $osSnapshotName  -DurationInSecond $sasExpiryDuration -Access Read -Verbose
-    #Write-Host ("SAS Token: {0}" -f $osSAS.AccessSAS)    
-    Write-Host ("SAS Token: {0}" -f $osSAS)    
-    $progress = Start-AzureStorageBlobCopy -AbsoluteUri $osSAS -DestContainer $storageContainerName -DestContext $destinationContext -DestBlob $osVHDFileName -Force
+#    Write-Host ("SAS Token: {0}" -f $osSAS.AccessSAS)    
+ #   Write-Host ("SAS Token: {0}" -f $osSAS)    
+    $progress = Start-AzureStorageBlobCopy -AbsoluteUri $sasUri -DestContainer $storageContainerName -DestContext $destinationContext -DestBlob $osVHDFileName -Force
    
     while (($progress | Get-AzureStorageBlobCopyState).Status -eq "Pending") {
         Start-Sleep -s 60
@@ -68,6 +77,17 @@ foreach ($region in $regions) {
     }
     else {
         $message = ("Error copying OS disk to region {0}" -f $configRegion.location)
+        Write-Host $message -ForegroundColor Red
+        Add-Content -Path (Join-Path $PWD "vhdcreation.log") -Value $message -Force
+    }
+    
+    $result = ($progress | Get-AzureStorageBlobCopyState)
+    
+    if ($result.Status -eq "Success") {
+        Write-Host ("Successful copy of Data Disk to {0}" -f $configRegion.location) -ForegroundColor Green
+    }
+    else {
+        $message = ("Error copying data disk to region {0}" -f $configRegion.location)
         Write-Host $message -ForegroundColor Red
         Add-Content -Path (Join-Path $PWD "vhdcreation.log") -Value $message -Force
     }
