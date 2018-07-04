ad1da7c343e1fc7621b7cf7fa1fb9b6ea70fb0f4
J.F. Larente <jfl@sitecore.net>
2018-07-04 10:45:52 -0400

    Updating to latest version

--- XC/install/set-installation-defaults.ps1
+++ XC/install/set-installation-defaults.ps1
@@ -21,23 +21,23 @@ $assets.downloadFolder = Join-Path $assets.root "Downloads"
 #Commerce
 $assets.commerce.nugetPackageLocation = "http://nuget1ca2/nuget/Commerce/"
 $assets.commerce.nugetPackageName = "Sitecore.Commerce.ReleasePackage.Content"
-$assets.commerce.nugetPackageVersion = "2.2.63"
+$assets.commerce.nugetPackageVersion = "2.2.124"
 $assets.commerce.packageUrl = "https://v9assets.blob.core.windows.net/v9-onprem-assets/Sitecore.Commerce.2018.05-2.2.63.zip?sv=2017-04-17&ss=bfqt&srt=sco&sp=rwdlacup&se=2027-11-09T20%3A11%3A50Z&st=2017-11-09T12%3A11%3A50Z&spr=https&sig=naspk%2BQflDLjyuC6gfXw4OZKvhhxzTlTvDctfw%2FByj8%3D"
 $assets.commerce.installationFolder = Join-Path $assets.root "Commerce"
 
 
 #Commerce Files to Extract
 $sifCommerceVersion = $assets.commerce.filesToExtract | Where-Object { $_.name -eq "SIF.Sitecore.Commerce"} 
-$sifCommerceVersion.version = "1.2.8"
+$sifCommerceVersion.version = "1.2.14"
 $assets.commerce.sifCommerceRoot = Join-Path $assets.commerce.installationFolder $($sifCommerceVersion.name + "." + $sifCommerceVersion.version)
 $commerceEngineVersion = $assets.commerce.filesToExtract | Where-Object { $_.name -eq "Sitecore.Commerce.Engine"} 
-$commerceEngineVersion.version = "2.2.63"
+$commerceEngineVersion.version = "2.2.124"
 
 $commerceEngineSDKVersion = $assets.commerce.filesToExtract | Where-Object { $_.name -eq "Sitecore.Commerce.Engine.SDK"} 
-$commerceEngineSDKVersion.version = "2.2.34"
+$commerceEngineSDKVersion.version = "2.2.72"
 
 $bizFxVersion = $assets.commerce.filesToExtract | Where-Object { $_.name -eq "Sitecore.BizFX"} 
-$bizFxVersion.version = "1.2.13"
+$bizFxVersion.version = "1.2.19"
 
 # Settings
 $site = $json.settings.site
