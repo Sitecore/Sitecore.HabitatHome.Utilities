08c14db0a1e5185c7750b46b8f93b386ad11e4b8
Jean-François Larente <jfl@sitecore.net>
2018-07-26 17:04:11 +0000

    Merged PR 874: back-merging master to develop for 9.0.2

--- XC/install/get-latest-commerce.ps1
+++ XC/install/get-latest-commerce.ps1
@@ -2,7 +2,7 @@
 Param(
     $DownloadFolder = "",
     $CommerceAssetFolder = "",
-    $CommercePackageUrl = "https://v9assets.blob.core.windows.net/v9-onprem-assets/Sitecore.Commerce.2018.03-2.1.55.zip?sv=2017-04-17&ss=bfqt&srt=sco&sp=rwdlacup&se=2027-11-09T20%3A11%3A50Z&st=2017-11-09T12%3A11%3A50Z&spr=https&sig=naspk%2BQflDLjyuC6gfXw4OZKvhhxzTlTvDctfw%2FByj8%3D"
+    $CommercePackageUrl = "https://v9assets.blob.core.windows.net/v9-onprem-assets/Sitecore.Commerce.2018.07-2.2.124.zip?sv=2017-04-17&ss=bfqt&srt=sco&sp=rwdlacup&se=2027-11-09T20%3A11%3A50Z&st=2017-11-09T12%3A11%3A50Z&spr=https&sig=naspk%2BQflDLjyuC6gfXw4OZKvhhxzTlTvDctfw%2FByj8%3D"
 
 )
 if ($DownloadFolder -eq "") {
@@ -44,10 +44,10 @@ $commercePackagePaths = $CommercePackageUrl.Split("?")
 $commercePackageFileName = $commercePackagePaths[0].substring($commercePackagePaths[0].LastIndexOf("/") + 1)
 $commercePackageDestination = $([io.path]::combine($DownloadFolder, $commercePackageFileName)).ToString()
 
-Write-Host "Saving $CommercePackageUrl to $commercePackageDestination - if required" -ForegroundColor Green
-if (!(Test-Path $commercePackageDestination)) {
-    Start-BitsTransfer -Source $CommercePackageUrl -Destination $commercePackageDestination
-}
+#Write-Host "Saving $CommercePackageUrl to $commercePackageDestination - if required" -ForegroundColor Green
+#if (!(Test-Path $commercePackageDestination)) {
+#    Start-BitsTransfer -Source $CommercePackageUrl -Destination $commercePackageDestination
+#}
 
 Write-Host "Extracting to $($CommerceAssetFolder)"
 set-alias sz "$env:ProgramFiles\7-zip\7z.exe"
