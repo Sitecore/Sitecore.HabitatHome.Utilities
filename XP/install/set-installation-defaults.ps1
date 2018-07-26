4854b2275a3b5695fbefef9d4ca8fe8fbf8ca781
Jean-François Larente <jfl@sitecore.net>
2018-07-26 17:04:11 +0000

    Merged PR 874: back-merging master to develop for 9.0.2

--- XP/install/set-installation-defaults.ps1
+++ XP/install/set-installation-defaults.ps1
@@ -6,15 +6,15 @@ Write-Host "Setting Defaults and creating $ConfigurationFile"
 
 $json = Get-Content -Raw .\install-settings.json -Encoding Ascii |  ConvertFrom-Json
 
-# Assets and prerequisites
+Write-host "Setting default 'Assets and prerequisites' parameters"
 
 $assets = $json.assets
 $assets.root = "$PSScriptRoot\assets"
 $assets.psRepository = "https://sitecore.myget.org/F/sc-powershell/api/v2/"
 $assets.psRepositoryName = "SitecoreGallery"
 $assets.licenseFilePath = Join-Path $assets.root "license.xml"
-$assets.sitecoreVersion = "9.0.1 rev. 171219"
-$assets.installerVersion = "1.1.0"
+$assets.sitecoreVersion = "9.0.2 rev. 180604"
+$assets.installerVersion = "1.2.0"
 $assets.certificatesPath = Join-Path $assets.root "Certificates"
 $assets.jreRequiredVersion = "8.0.1510"
 $assets.dotnetMinimumVersionValue = "394802"
@@ -22,17 +22,19 @@ $assets.dotnetMinimumVersion = "4.6.2"
 $assets.installPackagePath = Join-Path $assets.root "installpackage.aspx"
 
 # Settings
-
+Write-Host "Setting default 'Site' parameters"
 # Site Settings
 $site = $json.settings.site
-$site.prefix = "habitat"
+$site.prefix = "habitathome"
 $site.suffix = "dev.local"
 $site.webroot = "C:\inetpub\wwwroot"
 $site.hostName = $json.settings.site.prefix + "." + $json.settings.site.suffix
-$site.habitatHomePrefix = $json.settings.site.prefix + "home"
-$site.habitatHomeHostName = $json.settings.habitatHomePrefix + "." + $json.settings.site.suffix
-$site.habitatHomeConfigurationPath = (Get-ChildItem $pwd -filter "habitathome-xp0.json" -Recurse).FullName
+$site.enableInstallationImprovements = (Get-ChildItem $pwd -filter "enable-installation-improvements.json" -Recurse).FullName
+$site.disableInstallationImprovements = (Get-ChildItem $pwd -filter "disable-installation-improvements.json" -Recurse).FullName
+$site.configureSearchIndexes = (Get-ChildItem $pwd -filter "configure-search-indexes.json" -Recurse).FullName
 $site.habitatHomeSslCertificateName = $site.prefix + "." + $site.suffix
+
+Write-Host "Setting default 'SQL' parameters"
 $sql = $json.settings.sql
 # SQL Settings
 $sql.server = "."
@@ -40,6 +42,7 @@ $sql.adminUser = "sa"
 $sql.adminPassword = "12345"
 $sql.minimumVersion="13.0.4001"
 
+Write-Host "Setting default 'xConnect' parameters"
 # XConnect Parameters
 $xConnect = $json.settings.xConnect
 
@@ -53,6 +56,7 @@ $xConnect.siteRoot = Join-Path $site.webRoot -ChildPath $xConnect.siteName
 $xConnect.sqlCollectionUser = $site.prefix + "collectionuser"
 $xConnect.sqlCollectionPassword = "Test12345"
 
+Write-Host "Setting default 'Sitecore' parameters"
 
 # Sitecore Parameters
 $sitecore = $json.settings.sitecore
@@ -63,19 +67,22 @@ $sitecore.sslConfigurationPath = "$PSScriptRoot\certificates\sitecore-ssl.json"
 $sitecore.packagePath = Join-Path $assets.root $("Sitecore " + $assets.sitecoreVersion +" (OnPrem)_single.scwdp.zip")
 $sitecore.siteRoot = Join-Path $site.webRoot -ChildPath $site.hostName
 
+Write-Host "Setting default 'Solr' parameters"
 # Solr Parameters
 $solr = $json.settings.solr
 $solr.url = "https://localhost:8983/solr"
 $solr.root = "c:\solr"
 $solr.serviceName = "Solr"
+
+Write-Host "Setting default 'modules' parameters"
 $modules = $json.modules
 
 $spe = $modules | Where-Object { $_.id -eq "spe"}
-$spe.packagePath = Join-Path $assets.root "packages\Sitecore PowerShell Extensions-4.7 for Sitecore 8.zip"
+$spe.packagePath = Join-Path $assets.root "packages\Sitecore PowerShell Extensions-4.7.2 for Sitecore 8.zip"
 $spe.install = $true
 
 $sxa = $modules | Where-Object { $_.id -eq "sxa"}
-$sxa.packagePath = Join-Path $assets.root "packages\Sitecore Experience Accelerator 1.6 rev. 180103 for 9.0.zip"
+$sxa.packagePath = Join-Path $assets.root "packages\Sitecore Experience Accelerator 1.7.1 rev. 180604 for 9.0.zip"
 $sxa.install = $true
 
 $def = $modules | Where-Object {$_.id -eq "def"}
@@ -102,5 +109,6 @@ $defDynamicsConnect = $modules | Where-Object {$_.id -eq "defDynamicsConnect"}
 $defDynamicsConnect.packagePath = Join-Path $assets.root "packages\Connect for Microsoft Dynamics 2.0.1 rev. 180108.zip"
 $defDynamicsConnect.install = $true
 
+Write-Host ("Saving Configuration file to {0}" -f $ConfigurationFile)
 
 Set-Content $ConfigurationFile  (ConvertTo-Json -InputObject $json -Depth 3 )
