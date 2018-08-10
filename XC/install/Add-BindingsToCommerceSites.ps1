6bc689bdc7463e1c2ab8d2e53fd86845aff5ff85
Jean-François Larente <jfl@sitecore.net>
2018-08-10 13:49:06 +0000

    Merged PR 924: merging latest changes from github
    - Added VM size as parameter
    - Fixed AzureRM module bug (must specify version 6.3.0 or earlier)
    - Added ability to change hostname with new (or existing) certificate
    - Finished deploy-commerce-engine functionality to update IdServer and BizFx settings
    - Added functionality to automatically install prerequisites
    - Added download Sitecore (XP+XC) functionality
    - - Refactor Azure scripts and add versioning
    - Improve Add Bindings scripts
    - Added ability to also add HTTP binding and / or SslOnly
    - merging develop

---
+++ XC/install/Add-BindingsToCommerceSites.ps1
@@ -0,0 +1,23 @@
+Param(
+    [string] $hostName = "my.hostname",
+    [string]$CertificateName = "my.hostname",
+    [string[]]$sites = @("habitathome.dev.local:443", "CommerceAuthoring_habitathome:5000", "CommerceMinions_habitathome:5010", "CommerceOps_habitathome:5015", "CommerceShops_habitathome:5020", "SitecoreBizFx:4200", "SitecoreIdentityServer:5050")
+)
+
+Set-Location Resolve-Path "..\..\XP\Install"
+
+foreach ($site in $sites) {
+    $siteName = $site.split(":")[0]
+    $port = $site.split(":")[1]
+
+    #   Remove -SkipCreateCert if you would like to create a new certificate
+
+    if ($siteName -ne "habitathome.dev.local") {
+
+        .\Add-SSLSiteBindingWithCertificate.ps1 -SiteName $siteName -Port $port -HostName $hostName -CertificateName $CertificateName -SkipCreateCert -SslOnly
+    }
+    else {
+        .\Add-SSLSiteBindingWithCertificate.ps1 -SiteName $siteName -Port $port -HostName $hostName -CertificateName $CertificateName -SkipCreateCert
+
+    }
+}
