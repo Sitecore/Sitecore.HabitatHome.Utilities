4148eb5fdf82fc6a7892aae46c0822824569ca85
Jean-François Larente <jfl@sitecore.net>
2019-01-24 06:32:07 -0500

    - Additional cleanup

--- XP/install/install-modules.ps1
+++ XP/install/install-modules.ps1
@@ -43,19 +43,9 @@ $sitecore = $config.settings.sitecore
 $solr = $config.settings.solr
 $sql = $config.settings.sql
 $xConnect = $config.settings.xConnect
-$resourcePath = Join-Path $assets.root "configuration"
 $sharedResourcePath = Join-Path $assets.sharedUtilitiesRoot "assets\configuration"
 $downloadFolder = $assets.root
 $packagesFolder = (Join-Path $downloadFolder "packages")
-
-
-# Write-Host "Setting Modules Path" -ForegroundColor Green
-# $modulesPath = ( Join-Path -Path $assets.sharedUtilitiesRoot -ChildPath "assets\Modules" )
-# if ($env:PSModulePath -notlike "*$modulesPath*") {
-#     $p = $env:PSModulePath + ";" + $modulesPath
-#     [Environment]::SetEnvironmentVariable("PSModulePath", $p)
-# }
-
 
 $loginSession = $null
 
@@ -179,110 +169,12 @@ Function Install-Modules {
         SitecoreAdminPassword           = $sitecore.adminPassword
     }
     Push-Location $sharedResourcePath
-    Install-SitecoreConfiguration @params -Verbose *>&1 | Tee-Object "C:\projects\Demo.Utilities.VSTS\XP\Install\output.log"
+    Install-SitecoreConfiguration @params  *>&1 | Tee-Object "C:\projects\Demo.Utilities.VSTS\XP\Install\output.log"
     Pop-Location
    
 }
 
 
-Function Enable-ContainedDatabases {
-    #Enable Contained Databases
-    Write-Host "Enable contained databases" -ForegroundColor Green
-    $params = @{
-        Path             = (Join-Path $sharedResourcePath "enable-contained-databases.json")
-        SqlServer        = $sql.server
-        SqlAdminUser     = $sql.adminUser 
-        SqlAdminPassword = $sql.adminPassword
-    }
-    Install-SitecoreConfiguration @params  -WorkingDirectory $(Join-Path $PWD "logs")
-}
-Function Add-DatabaseUsers {
-    Write-Host ("Adding {0}" -f $sql.coreUser) -ForegroundColor Green
-    $sqlVariables = "DatabasePrefix = $($site.prefix)", "DatabaseSuffix = Core", "UserName = $($sql.coreUser)", "Password = $($sql.corePassword)"
-
-    $params = @{
-        Path             = (Join-Path $sharedResourcePath "execute-sql-script.json")
-        SqlServer        = $sql.server
-        SqlAdminUser     = $sql.adminUser 
-        SqlAdminPassword = $sql.adminPassword
-        ScriptFile       = Join-Path $assets.sharedUtilitiesRoot "\database\addcoredatabaseuser.sql"
-        SqlVariables     = $sqlVariables
-    }
-
-    Install-SitecoreConfiguration @params  -WorkingDirectory $(Join-Path $PWD "logs")
-  
-    
-    Write-Host ("Adding {0}" -f $sql.securityuser) -ForegroundColor Green
-    $sqlVariables = "DatabasePrefix = $($site.prefix)", "DatabaseSuffix = Core", "UserName = $($sql.securityUser)", "Password = $($sql.securityPassword)"
-
-    $params = @{
-        Path             = (Join-Path $sharedResourcePath "execute-sql-script.json")
-        SqlServer        = $sql.server
-        SqlAdminUser     = $sql.adminUser 
-        SqlAdminPassword = $sql.adminPassword
-        ScriptFile       = Join-Path $assets.sharedUtilitiesRoot "\database\addcoredatabaseuser.sql" 
-        SqlVariables     = $sqlVariables
-    }
-    
-    Install-SitecoreConfiguration @params  -WorkingDirectory $(Join-Path $PWD "logs")
-      
-   
-    Write-Host ("Adding {0}" -f $sql.masterUser) -ForegroundColor Green
-    
-    $sqlVariables = "DatabasePrefix = $($site.prefix)", "DatabaseSuffix = Master", "UserName = $($sql.masterUser)", "Password = $($sql.masterPassword)"
-    $params = @{
-        Path             = (Join-Path $sharedResourcePath "execute-sql-script.json")
-        SqlServer        = $sql.server
-        SqlAdminUser     = $sql.adminUser 
-        SqlAdminPassword = $sql.adminPassword
-        ScriptFile       = Join-Path $assets.sharedUtilitiesRoot "database\adddatabaseuser.sql" 
-        SqlVariables     = $sqlVariables
-    }
-        
-    Install-SitecoreConfiguration @params  -WorkingDirectory $(Join-Path $PWD "logs")
-      
-}
-function Update-SXASolrCores {
-    try {
-        $sxa = $modules | Where-Object { $_.id -eq "sxa"}
-        if ($false -eq $sxa.install) {
-            return
-        }
-        $params = @{
-            Path        = Join-Path $resourcePath "SXA\configure-search-indexes.json"
-            InstallDir  = Join-Path $site.webRoot $site.hostName
-            ResourceDir = $($assets.root + "\\configuration")
-            SitePrefix  = $site.prefix
-        }
-        Install-SitecoreConfiguration @params -WorkingDirectory $(Join-Path $PWD "logs")
-    }
-    catch {
-        write-host "$site.hostName Failed to updated search index configuration" -ForegroundColor Red
-        throw
-    }
-    # Install SXA Solr Cores
-    
-    $sxaSolrConfigPath = Join-Path $resourcePath 'SXA\sxa-solr-config.json'
-    
-    try {
-        $params = @{
-            Path                  = Join-path $resourcePath 'SXA\sxa-solr.json'
-            SolrUrl               = $solr.url 
-            SolrRoot              = $solr.root 
-            SolrService           = $solr.serviceName 
-            CorePrefix            = $site.prefix
-            SXASolrConfigPath     = $sxaSolrConfigPath
-            SiteName              = $site.hostName
-            SitecoreAdminPassword = $sitecore.adminPassword
- 
-        }
-        Install-SitecoreConfiguration @params -WorkingDirectory $(Join-Path $PWD "logs")
-    }
-    catch {
-        write-host "SXA SOLR Failed" -ForegroundColor Red
-        throw
-    }
-}
 
 Function Start-Services {
     IISRESET /START
@@ -293,10 +185,7 @@ Function Start-Services {
 }
 Import-Module (Join-Path $assets.sharedUtilitiesRoot "assets\modules\SharedInstallationUtilities\SharedInstallationUtilities.psm1") -Verbose -Force
 Install-SitecoreInstallFramework
-#Install-SitecoreAzureToolkit
+Install-SitecoreAzureToolkit
 Install-Modules
-#Add-DatabaseUsers
-#Start-Services
-#Update-SXASolrCores
 $StopWatch.Stop()
 $StopWatch
