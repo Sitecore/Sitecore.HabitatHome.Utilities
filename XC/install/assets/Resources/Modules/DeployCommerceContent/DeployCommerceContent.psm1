cbb6528cf9aea61e764460166f071ccf88dd9d62
Jean-François Larente <jfl@sitecore.net>
2019-01-28 14:40:03 -0500

    - Refactored module installation to use a shared merge-webconfig.json config - Updated to build 103 - Removed dependency on Commerce IdentityServer - ISSUE - Marketing Automation Service cannot access cert store

--- XC/install/assets/Resources/Modules/DeployCommerceContent/DeployCommerceContent.psm1
+++ XC/install/assets/Resources/Modules/DeployCommerceContent/DeployCommerceContent.psm1
@@ -9,7 +9,9 @@ Function Invoke-DeployCommerceContentTask {
         [Parameter(Mandatory = $true)]
         [string]$PhysicalPath,        
         [Parameter(Mandatory = $true)]
-        [string]$UserName, 
+        [string]$UserName,
+        [Parameter(Mandatory = $true)]
+        [string]$SitecoreIdentityServerUrl, 
         [Parameter(Mandatory = $false)]
         [psobject[]]$BraintreeAccount = @(),        
         [Parameter(Mandatory = $false)]
@@ -41,7 +43,7 @@ Function Invoke-DeployCommerceContentTask {
         [Parameter(Mandatory = $false)]
         [string]$AzureSearchAdminKey,		
         [Parameter(Mandatory = $false)]
-        [string]$AzureSearchQueryKey		
+        [string]$AzureSearchQueryKey
     )
 
     try {       
@@ -77,6 +79,7 @@ Function Invoke-DeployCommerceContentTask {
                 # Set the proper environment name                
                 $pathToJson = $(Join-Path -Path $PhysicalPath -ChildPath "wwwroot\config.json")
                 $originalJson = Get-Content $pathToJson -Raw  | ConvertFrom-Json
+                $originalJson.AppSettings.SitecoreIdentityServerUrl = $SitecoreIdentityServerUrl
                 $originalJson.AppSettings.EnvironmentName = "$($EnvironmentsPrefix)Authoring"
                 $allowedOrigins = @("https://localhost:$SitecoreBizFxPort", "https://$SiteHostHeaderName")
                 $originalJson.AppSettings.AllowedOrigins = $allowedOrigins 
@@ -224,7 +227,6 @@ Function Invoke-DeployCommerceContentTask {
                 # Copy the the CommerceServices files to the $Name Services
                 Write-Host "Copying Commerce Services from $global:opsServicePath to $PhysicalPath" -ForegroundColor Yellow ;
                 Get-ChildItem $global:opsServicePath | Copy-item -Destination $PhysicalPath -Container -Recurse
-
                 Write-Host "$($_) Services extraction completed" -ForegroundColor Green ;
 
                 $commerceServicesLogDir = $(Join-Path -Path $PhysicalPath -ChildPath "wwwroot\logs")
@@ -248,69 +250,9 @@ Function Invoke-DeployCommerceContentTask {
                     $environment = "$($EnvironmentsPrefix)Minions"
                 }		
                 $originalJson.AppSettings.EnvironmentName = $environment
+                $originalJson.AppSettings.SitecoreIdentityServerUrl = $SitecoreIdentityServerUrl
                 $originalJson | ConvertTo-Json -Depth 100 -Compress | set-content $pathToJson
-            }               
-            {($_ -match "IdentityServer")} {
-                Write-Host
-                # Extracting Sitecore.IdentityServer zip file
-                $commerceServicesItem = Get-Item $ServicesContentPath | select-object -first 1
-
-                if (Test-Path $commerceServicesItem -PathType Leaf) {
-                    # Assume path to zip file passed in, extract zip to $PhysicalPath
-                    #Extracting the CommerceServices zip file Commerce Shops Services
-                    Write-Host "Extracting Sitecore.IdentityServer from $commerceServicesItem to $PhysicalPath" -ForegroundColor Yellow ;
-                    Expand-Archive $commerceServicesItem -DestinationPath $PhysicalPath -Force
-                    Write-Host "Sitecore.IdentityServer extraction completed" -ForegroundColor Green ;
-                }
-                else {
-                    # Assume path is to a published Sitecore Identity Server folder. Copy the contents of the folder to the $PhysicalPath
-                    Write-Host "Copying the contents of Sitecore Identity Server from $commerceServicesItem to $PhysicalPath" -ForegroundColor Yellow ;
-                    Get-ChildItem $ServicesContentPath | Copy-item -Destination $PhysicalPath -Container -Recurse
-                    Write-Host "Sitecore.IdentityServer copy completed" -ForegroundColor Green ;
-                }
-
-                $commerceServicesLogDir = $(Join-Path -Path $PhysicalPath -ChildPath "wwwroot\logs")
-                if (-not (Test-Path -Path $commerceServicesLogDir)) {                      
-                    Write-Host "Creating Commerce Services logs directory at: $commerceServicesLogDir"
-                    New-Item -Path $PhysicalPath -Name "wwwroot\logs" -ItemType "directory"
-                }
-				
-                Write-Host "Granting full access to $UserName to logs directory: $commerceServicesLogDir"
-                GrantFullReadWriteAccessToFile -Path $commerceServicesLogDir  -UserName "$($UserName)"
-				
-                $appSettingsPath = $(Join-Path -Path $PhysicalPath -ChildPath "wwwroot\appsettings.json")
-                $originalJson = Get-Content $appSettingsPath -Raw | ConvertFrom-Json
-                $connectionString = $originalJson.AppSettings.SitecoreMembershipOptions.ConnectionString
-                $connectionString = $connectionString -replace "SXAStorefrontSitecore_Core", $SitecoreCoreDbName
-                $connectionString = $connectionString -replace "localhost", $SitecoreDbServer
-                $originalJson.AppSettings.SitecoreMembershipOptions.ConnectionString = $connectionString
-
-                if ($SitecoreBizFxPort -ne "4200") {
-                    $client = $originalJson.AppSettings.Clients[0]
-                    $Uris = @()
-                    for ($i = 0; $i -lt $client.RedirectUris.Count ; $i++) {
-                        $find = $originalJson.AppSettings.Clients.RedirectUris[$i]
-                        $Uris += $find -replace "4200", $SitecoreBizFxPort                            
-                    }
-                    $originalJson.AppSettings.Clients[0].RedirectUris = $Uris
-
-                    $Uris1 = @()
-                    for ($i = 0; $i -lt $client.PostLogoutRedirectUris.Count ; $i++) {
-                        $find = $originalJson.AppSettings.Clients.PostLogoutRedirectUris[$i]
-                        $Uris1 += $find -replace "4200", $SitecoreBizFxPort                            
-                    }
-                    $originalJson.AppSettings.Clients[0].PostLogoutRedirectUris = $Uris1
-					  
-                    $Uris2 = @()
-                    for ($i = 0; $i -lt $client.AllowedCorsOrigins.Count ; $i++) {
-                        $find = $originalJson.AppSettings.Clients.AllowedCorsOrigins[$i]
-                        $Uris2 += $find -replace "4200", $SitecoreBizFxPort                            
-                    }
-                    $originalJson.AppSettings.Clients[0].AllowedCorsOrigins = $Uris2
-                }
-
-                $originalJson | ConvertTo-Json -Depth 100 -Compress | set-content $appSettingsPath						
-            }
+            }     
             {($_ -match "BizFx")} {
                 Write-Host
                 # Copying the BizFx content
@@ -333,6 +275,7 @@ Function Invoke-DeployCommerceContentTask {
                 $originalJson = Get-Content $pathToJson -Raw  | ConvertFrom-Json
                 $originalJson.EngineUri = $originalJson.EngineUri -replace "5000", $CommerceAuthoringServicesPort
                 $originalJson.BizFxUri = $originalJson.BizFxUri -replace "4200", $SitecoreBizFxPort  
+                $originalJson.IdentityServerUri = $SitecoreIdentityServerUrl
                   
                 $originalJson | ConvertTo-Json -Depth 100 -Compress | set-content $pathToJson                			
             }			
@@ -460,132 +403,3 @@ function GrantFullReadWriteAccessToFile {
         }
     }
 }
-# SIG # Begin signature block
-# MIIXwQYJKoZIhvcNAQcCoIIXsjCCF64CAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
-# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQU7nLq3Y1YO+2siU1IYo7ksJBW
-# AYmgghL8MIID7jCCA1egAwIBAgIQfpPr+3zGTlnqS5p31Ab8OzANBgkqhkiG9w0B
-# AQUFADCBizELMAkGA1UEBhMCWkExFTATBgNVBAgTDFdlc3Rlcm4gQ2FwZTEUMBIG
-# A1UEBxMLRHVyYmFudmlsbGUxDzANBgNVBAoTBlRoYXd0ZTEdMBsGA1UECxMUVGhh
-# d3RlIENlcnRpZmljYXRpb24xHzAdBgNVBAMTFlRoYXd0ZSBUaW1lc3RhbXBpbmcg
-# Q0EwHhcNMTIxMjIxMDAwMDAwWhcNMjAxMjMwMjM1OTU5WjBeMQswCQYDVQQGEwJV
-# UzEdMBsGA1UEChMUU3ltYW50ZWMgQ29ycG9yYXRpb24xMDAuBgNVBAMTJ1N5bWFu
-# dGVjIFRpbWUgU3RhbXBpbmcgU2VydmljZXMgQ0EgLSBHMjCCASIwDQYJKoZIhvcN
-# AQEBBQADggEPADCCAQoCggEBALGss0lUS5ccEgrYJXmRIlcqb9y4JsRDc2vCvy5Q
-# WvsUwnaOQwElQ7Sh4kX06Ld7w3TMIte0lAAC903tv7S3RCRrzV9FO9FEzkMScxeC
-# i2m0K8uZHqxyGyZNcR+xMd37UWECU6aq9UksBXhFpS+JzueZ5/6M4lc/PcaS3Er4
-# ezPkeQr78HWIQZz/xQNRmarXbJ+TaYdlKYOFwmAUxMjJOxTawIHwHw103pIiq8r3
-# +3R8J+b3Sht/p8OeLa6K6qbmqicWfWH3mHERvOJQoUvlXfrlDqcsn6plINPYlujI
-# fKVOSET/GeJEB5IL12iEgF1qeGRFzWBGflTBE3zFefHJwXECAwEAAaOB+jCB9zAd
-# BgNVHQ4EFgQUX5r1blzMzHSa1N197z/b7EyALt0wMgYIKwYBBQUHAQEEJjAkMCIG
-# CCsGAQUFBzABhhZodHRwOi8vb2NzcC50aGF3dGUuY29tMBIGA1UdEwEB/wQIMAYB
-# Af8CAQAwPwYDVR0fBDgwNjA0oDKgMIYuaHR0cDovL2NybC50aGF3dGUuY29tL1Ro
-# YXd0ZVRpbWVzdGFtcGluZ0NBLmNybDATBgNVHSUEDDAKBggrBgEFBQcDCDAOBgNV
-# HQ8BAf8EBAMCAQYwKAYDVR0RBCEwH6QdMBsxGTAXBgNVBAMTEFRpbWVTdGFtcC0y
-# MDQ4LTEwDQYJKoZIhvcNAQEFBQADgYEAAwmbj3nvf1kwqu9otfrjCR27T4IGXTdf
-# plKfFo3qHJIJRG71betYfDDo+WmNI3MLEm9Hqa45EfgqsZuwGsOO61mWAK3ODE2y
-# 0DGmCFwqevzieh1XTKhlGOl5QGIllm7HxzdqgyEIjkHq3dlXPx13SYcqFgZepjhq
-# IhKjURmDfrYwggSjMIIDi6ADAgECAhAOz/Q4yP6/NW4E2GqYGxpQMA0GCSqGSIb3
-# DQEBBQUAMF4xCzAJBgNVBAYTAlVTMR0wGwYDVQQKExRTeW1hbnRlYyBDb3Jwb3Jh
-# dGlvbjEwMC4GA1UEAxMnU3ltYW50ZWMgVGltZSBTdGFtcGluZyBTZXJ2aWNlcyBD
-# QSAtIEcyMB4XDTEyMTAxODAwMDAwMFoXDTIwMTIyOTIzNTk1OVowYjELMAkGA1UE
-# BhMCVVMxHTAbBgNVBAoTFFN5bWFudGVjIENvcnBvcmF0aW9uMTQwMgYDVQQDEytT
-# eW1hbnRlYyBUaW1lIFN0YW1waW5nIFNlcnZpY2VzIFNpZ25lciAtIEc0MIIBIjAN
-# BgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAomMLOUS4uyOnREm7Dv+h8GEKU5Ow
-# mNutLA9KxW7/hjxTVQ8VzgQ/K/2plpbZvmF5C1vJTIZ25eBDSyKV7sIrQ8Gf2Gi0
-# jkBP7oU4uRHFI/JkWPAVMm9OV6GuiKQC1yoezUvh3WPVF4kyW7BemVqonShQDhfu
-# ltthO0VRHc8SVguSR/yrrvZmPUescHLnkudfzRC5xINklBm9JYDh6NIipdC6Anqh
-# d5NbZcPuF3S8QYYq3AhMjJKMkS2ed0QfaNaodHfbDlsyi1aLM73ZY8hJnTrFxeoz
-# C9Lxoxv0i77Zs1eLO94Ep3oisiSuLsdwxb5OgyYI+wu9qU+ZCOEQKHKqzQIDAQAB
-# o4IBVzCCAVMwDAYDVR0TAQH/BAIwADAWBgNVHSUBAf8EDDAKBggrBgEFBQcDCDAO
-# BgNVHQ8BAf8EBAMCB4AwcwYIKwYBBQUHAQEEZzBlMCoGCCsGAQUFBzABhh5odHRw
-# Oi8vdHMtb2NzcC53cy5zeW1hbnRlYy5jb20wNwYIKwYBBQUHMAKGK2h0dHA6Ly90
-# cy1haWEud3Muc3ltYW50ZWMuY29tL3Rzcy1jYS1nMi5jZXIwPAYDVR0fBDUwMzAx
-# oC+gLYYraHR0cDovL3RzLWNybC53cy5zeW1hbnRlYy5jb20vdHNzLWNhLWcyLmNy
-# bDAoBgNVHREEITAfpB0wGzEZMBcGA1UEAxMQVGltZVN0YW1wLTIwNDgtMjAdBgNV
-# HQ4EFgQURsZpow5KFB7VTNpSYxc/Xja8DeYwHwYDVR0jBBgwFoAUX5r1blzMzHSa
-# 1N197z/b7EyALt0wDQYJKoZIhvcNAQEFBQADggEBAHg7tJEqAEzwj2IwN3ijhCcH
-# bxiy3iXcoNSUA6qGTiWfmkADHN3O43nLIWgG2rYytG2/9CwmYzPkSWRtDebDZw73
-# BaQ1bHyJFsbpst+y6d0gxnEPzZV03LZc3r03H0N45ni1zSgEIKOq8UvEiCmRDoDR
-# EfzdXHZuT14ORUZBbg2w6jiasTraCXEQ/Bx5tIB7rGn0/Zy2DBYr8X9bCT2bW+IW
-# yhOBbQAuOA2oKY8s4bL0WqkBrxWcLC9JG9siu8P+eJRRw4axgohd8D20UaF5Mysu
-# e7ncIAkTcetqGVvP6KUwVyyJST+5z3/Jvz4iaGNTmr1pdKzFHTx/kuDDvBzYBHUw
-# ggUrMIIEE6ADAgECAhAHplztCw0v0TJNgwJhke9VMA0GCSqGSIb3DQEBCwUAMHIx
-# CzAJBgNVBAYTAlVTMRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3
-# dy5kaWdpY2VydC5jb20xMTAvBgNVBAMTKERpZ2lDZXJ0IFNIQTIgQXNzdXJlZCBJ
-# RCBDb2RlIFNpZ25pbmcgQ0EwHhcNMTcwODIzMDAwMDAwWhcNMjAwOTMwMTIwMDAw
-# WjBoMQswCQYDVQQGEwJVUzELMAkGA1UECBMCY2ExEjAQBgNVBAcTCVNhdXNhbGl0
-# bzEbMBkGA1UEChMSU2l0ZWNvcmUgVVNBLCBJbmMuMRswGQYDVQQDExJTaXRlY29y
-# ZSBVU0EsIEluYy4wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC7PZ/g
-# huhrQ/p/0Cg7BRrYjw7ZMx8HNBamEm0El+sedPWYeAAFrjDSpECxYjvK8/NOS9dk
-# tC35XL2TREMOJk746mZqia+g+NQDPEaDjNPG/iT0gWsOeCa9dUcIUtnBQ0hBKsuR
-# bau3n7w1uIgr3zf29vc9NhCoz1m2uBNIuLBlkKguXwgPt4rzj66+18JV3xyLQJoS
-# 3ZAA8k6FnZltNB+4HB0LKpPmF8PmAm5fhwGz6JFTKe+HCBRtuwOEERSd1EN7TGKi
-# xczSX8FJMz84dcOfALxjTj6RUF5TNSQLD2pACgYWl8MM0lEtD/1eif7TKMHqaA+s
-# m/yJrlKEtOr836BvAgMBAAGjggHFMIIBwTAfBgNVHSMEGDAWgBRaxLl7Kgqjpepx
-# A8Bg+S32ZXUOWDAdBgNVHQ4EFgQULh60SWOBOnU9TSFq0c2sWmMdu7EwDgYDVR0P
-# AQH/BAQDAgeAMBMGA1UdJQQMMAoGCCsGAQUFBwMDMHcGA1UdHwRwMG4wNaAzoDGG
-# L2h0dHA6Ly9jcmwzLmRpZ2ljZXJ0LmNvbS9zaGEyLWFzc3VyZWQtY3MtZzEuY3Js
-# MDWgM6Axhi9odHRwOi8vY3JsNC5kaWdpY2VydC5jb20vc2hhMi1hc3N1cmVkLWNz
-# LWcxLmNybDBMBgNVHSAERTBDMDcGCWCGSAGG/WwDATAqMCgGCCsGAQUFBwIBFhxo
-# dHRwczovL3d3dy5kaWdpY2VydC5jb20vQ1BTMAgGBmeBDAEEATCBhAYIKwYBBQUH
-# AQEEeDB2MCQGCCsGAQUFBzABhhhodHRwOi8vb2NzcC5kaWdpY2VydC5jb20wTgYI
-# KwYBBQUHMAKGQmh0dHA6Ly9jYWNlcnRzLmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydFNI
-# QTJBc3N1cmVkSURDb2RlU2lnbmluZ0NBLmNydDAMBgNVHRMBAf8EAjAAMA0GCSqG
-# SIb3DQEBCwUAA4IBAQBozpJhBdsaz19E9faa/wtrnssUreKxZVkYQ+NViWeyImc5
-# qEZcDPy3Qgf731kVPnYuwi5S0U+qyg5p1CNn/WsvnJsdw8aO0lseadu8PECuHj1Z
-# 5w4mi5rGNq+QVYSBB2vBh5Ps5rXuifBFF8YnUyBc2KuWBOCq6MTRN1H2sU5LtOUc
-# Qkacv8hyom8DHERbd3mIBkV8fmtAmvwFYOCsXdBHOSwQUvfs53GySrnIYiWT0y56
-# mVYPwDj7h/PdWO5hIuZm6n5ohInLig1weiVDJ254r+2pfyyRT+02JVVxyHFMCLwC
-# ASs4vgbiZzMDltmoTDHz9gULxu/CfBGM0waMDu3cMIIFMDCCBBigAwIBAgIQBAkY
-# G1/Vu2Z1U0O1b5VQCDANBgkqhkiG9w0BAQsFADBlMQswCQYDVQQGEwJVUzEVMBMG
-# A1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3d3cuZGlnaWNlcnQuY29tMSQw
-# IgYDVQQDExtEaWdpQ2VydCBBc3N1cmVkIElEIFJvb3QgQ0EwHhcNMTMxMDIyMTIw
-# MDAwWhcNMjgxMDIyMTIwMDAwWjByMQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGln
-# aUNlcnQgSW5jMRkwFwYDVQQLExB3d3cuZGlnaWNlcnQuY29tMTEwLwYDVQQDEyhE
-# aWdpQ2VydCBTSEEyIEFzc3VyZWQgSUQgQ29kZSBTaWduaW5nIENBMIIBIjANBgkq
-# hkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA+NOzHH8OEa9ndwfTCzFJGc/Q+0WZsTrb
-# RPV/5aid2zLXcep2nQUut4/6kkPApfmJ1DcZ17aq8JyGpdglrA55KDp+6dFn08b7
-# KSfH03sjlOSRI5aQd4L5oYQjZhJUM1B0sSgmuyRpwsJS8hRniolF1C2ho+mILCCV
-# rhxKhwjfDPXiTWAYvqrEsq5wMWYzcT6scKKrzn/pfMuSoeU7MRzP6vIK5Fe7SrXp
-# dOYr/mzLfnQ5Ng2Q7+S1TqSp6moKq4TzrGdOtcT3jNEgJSPrCGQ+UpbB8g8S9MWO
-# D8Gi6CxR93O8vYWxYoNzQYIH5DiLanMg0A9kczyen6Yzqf0Z3yWT0QIDAQABo4IB
-# zTCCAckwEgYDVR0TAQH/BAgwBgEB/wIBADAOBgNVHQ8BAf8EBAMCAYYwEwYDVR0l
-# BAwwCgYIKwYBBQUHAwMweQYIKwYBBQUHAQEEbTBrMCQGCCsGAQUFBzABhhhodHRw
-# Oi8vb2NzcC5kaWdpY2VydC5jb20wQwYIKwYBBQUHMAKGN2h0dHA6Ly9jYWNlcnRz
-# LmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydEFzc3VyZWRJRFJvb3RDQS5jcnQwgYEGA1Ud
-# HwR6MHgwOqA4oDaGNGh0dHA6Ly9jcmw0LmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydEFz
-# c3VyZWRJRFJvb3RDQS5jcmwwOqA4oDaGNGh0dHA6Ly9jcmwzLmRpZ2ljZXJ0LmNv
-# bS9EaWdpQ2VydEFzc3VyZWRJRFJvb3RDQS5jcmwwTwYDVR0gBEgwRjA4BgpghkgB
-# hv1sAAIEMCowKAYIKwYBBQUHAgEWHGh0dHBzOi8vd3d3LmRpZ2ljZXJ0LmNvbS9D
-# UFMwCgYIYIZIAYb9bAMwHQYDVR0OBBYEFFrEuXsqCqOl6nEDwGD5LfZldQ5YMB8G
-# A1UdIwQYMBaAFEXroq/0ksuCMS1Ri6enIZ3zbcgPMA0GCSqGSIb3DQEBCwUAA4IB
-# AQA+7A1aJLPzItEVyCx8JSl2qB1dHC06GsTvMGHXfgtg/cM9D8Svi/3vKt8gVTew
-# 4fbRknUPUbRupY5a4l4kgU4QpO4/cY5jDhNLrddfRHnzNhQGivecRk5c/5CxGwcO
-# kRX7uq+1UcKNJK4kxscnKqEpKBo6cSgCPC6Ro8AlEeKcFEehemhor5unXCBc2XGx
-# DI+7qPjFEmifz0DLQESlE/DmZAwlCEIysjaKJAL+L3J+HNdJRZboWR3p+nRka7Lr
-# ZkPas7CM1ekN3fYBIM6ZMWM9CBoYs4GbT8aTEAb8B4H6i9r5gkn3Ym6hU/oSlBiF
-# LpKR6mhsRDKyZqHnGKSaZFHvMYIELzCCBCsCAQEwgYYwcjELMAkGA1UEBhMCVVMx
-# FTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UECxMQd3d3LmRpZ2ljZXJ0LmNv
-# bTExMC8GA1UEAxMoRGlnaUNlcnQgU0hBMiBBc3N1cmVkIElEIENvZGUgU2lnbmlu
-# ZyBDQQIQB6Zc7QsNL9EyTYMCYZHvVTAJBgUrDgMCGgUAoHAwEAYKKwYBBAGCNwIB
-# DDECMAAwGQYJKoZIhvcNAQkDMQwGCisGAQQBgjcCAQQwHAYKKwYBBAGCNwIBCzEO
-# MAwGCisGAQQBgjcCARUwIwYJKoZIhvcNAQkEMRYEFO8J+cIBkdNUGqa+YMqQYgng
-# zAjXMA0GCSqGSIb3DQEBAQUABIIBABpFmItHVHcSPA3ScZaC528194MmD9lBYmad
-# iWTDQqoXk+huDIgs5NOhY1yyxOGwykduZObSYssakg1XqKYmdz+Hj84mVr0NDfGP
-# sLxENK2VtKh1UR6TQQGoeoikY9Xv1mbA3bgi60L3TF691QvDhgl1izvB36hafIiB
-# bHyz2fZtAnOn4tDV+vEdh0zhN5KCHqmvTsXS3EjAG3xBqx2fHAxLAqpJFXy6YXZn
-# kdGfKQgubWpMD2HXpbk5culvHJJ4cPmuomN/J16uZ0uMd30CUrFYAo2hk5xBCUoU
-# obaqvwWAvQ3HnYrepAVxF5cByrikuZ9W0Hn5v+xbuWum5F8R7VihggILMIICBwYJ
-# KoZIhvcNAQkGMYIB+DCCAfQCAQEwcjBeMQswCQYDVQQGEwJVUzEdMBsGA1UEChMU
-# U3ltYW50ZWMgQ29ycG9yYXRpb24xMDAuBgNVBAMTJ1N5bWFudGVjIFRpbWUgU3Rh
-# bXBpbmcgU2VydmljZXMgQ0EgLSBHMgIQDs/0OMj+vzVuBNhqmBsaUDAJBgUrDgMC
-# GgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcN
-# MTkwMTA5MTYzNjUzWjAjBgkqhkiG9w0BCQQxFgQUhH0758qS6JW+X60GUNAP8wfy
-# xN4wDQYJKoZIhvcNAQEBBQAEggEAZ4E+M08dfXqtC0xdEfu9tViEg2kZowc+K1rQ
-# QX0+ei22tICQcw/AZD2LtnyA790ISZpWZPj0xxpUnxM0buSHTCJcL0zwOLMbXxJy
-# u4YSjOzr7O2XgfNVNyuk/IRYLJMw+2mJgcRuTw5sUQjxr3DjG3d4+JFCh9gtsUFM
-# G9rhUxJuxzYonO8KUqOzTKb7HJuYI94oUlkc4tQffbvTuz3o5ERzbQxBewyyY38/
-# kQ1day2bHcHPwPHLD6uQXMQKWH7x3dfSqTrGPKxwLlUTfhAzzVl5c8Yj48BC13gR
-# VDDM+jb2nMYaGH7DHH/UOZa/zFQaqPsdz9CIVP8Rfrvytz+kGQ==
-# SIG # End signature block
