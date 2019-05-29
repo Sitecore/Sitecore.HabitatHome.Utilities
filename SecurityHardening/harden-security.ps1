b7935f0f1e0f7f6a88137bd965c1fd3f0bae40e0
Jean-François Larente <jfl@sitecore.net>
2019-05-29 14:24:16 -0400

    Initial add of security hardening based on doc.sitecore.com

---
+++ SecurityHardening/harden-security.ps1
@@ -0,0 +1,54 @@
+Param(
+    [string]$siteName = "habitathome.dev.local",
+    [string]$webRoot = "C:\inetpub\wwwroot"
+)
+## Executes security hardening base on doc.sitecore.com recommendations
+## For more details, visit the Security Guide: https://doc.sitecore.com/developers/91/platform-administration-and-architecture/en/security-guide.html
+
+$configurationPath = Resolve-Path .\configuration
+$assetsPath = Resolve-Path .\assets
+
+$sharedModulesPath = Resolve-Path ..\Shared\assets\modules
+
+if ($env:PSModulePath -notlike "*$sharedModulesPath*") {
+    $p = $env:PSModulePath + ";" + $sharedModulesPath
+    [Environment]::SetEnvironmentVariable("PSModulePath", $p)
+}
+
+Import-Module SitecoreInstallFramework -RequiredVersion 2.1.0
+Import-Module SecurityHardening -Force -Verbose
+
+# Folders that will have the "Anonymous Authentication" feature disabled in IIS
+# ref: https://doc.sitecore.com/developers/90/platform-administration-and-architecture/en/deny-anonymous-users-access-to-a-folder.html
+$foldersToDenyAnonymousAccess = @('App_Config', 'sitecore/admin', 'sitecore/debug', 'sitecore/login', 'sitecore/shell/WebService')
+
+#contains the list of files to .disable
+# ref: https://doc.sitecore.com/developers/90/platform-administration-and-architecture/en/disable-administrative-tools.html
+$IOActionPath = Join-Path $assetsPath "ioactions.xml" 
+
+$uploadFilterPackageUri = "http://doc.sitecore.com/resources/upload-filter-1.0.0.2.zip"
+
+$workingFolder = Join-Path $assetsPath "work"
+
+if (!(Test-Path $workingFolder)) {
+    New-Item -Path $workingFolder -ItemType Directory -Force
+}
+
+$uploadFilterPackageFileName = "upload-filter-1.0.0.2.zip"
+
+$params = @{
+    WebRoot                     = $webRoot
+    SiteName                    = $siteName
+    FoldersToDisable            = $foldersToDenyAnonymousAccess   
+    IOActionPath                = $IOActionPath
+    UploadFilterPackageUri      = $uploadFilterPackageUri
+    UploadFilterPackageFileName = $uploadFilterPackageFileName
+    WorkingFolder               = $workingFolder
+    ConfigurationPatchPath      = Resolve-Path .\assets\configs
+    TransformSourcePath         = Resolve-Path .\assets\transforms
+}
+
+
+Push-Location $configurationPath
+Install-SitecoreConfiguration -Path .\harden-security.json @params -Verbose 
+Pop-Location
